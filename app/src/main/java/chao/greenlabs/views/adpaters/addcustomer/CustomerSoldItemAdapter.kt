8fbf8c431ef0aadf1200cfd6ed40d9460ee74deb
Cheng, Chao <chao.cheng@barco.com>
2021-03-10 14:50:59 +0800

    show the option in the manage market page

--- app/src/main/java/chao/greenlabs/views/adpaters/addcustomer/CustomerSoldItemAdapter.kt
+++ app/src/main/java/chao/greenlabs/views/adpaters/addcustomer/CustomerSoldItemAdapter.kt
@@ -33,10 +33,25 @@ class CustomerSoldItemAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
         val count = soldItem.count
 
         holder.itemView.tv_name.text = name
+        holder.itemView.tv_options.text = getSelectedOptions(soldItem)
         holder.itemView.tv_price.text = context.getString(R.string.price, price)
         holder.itemView.tv_count.text = count.toString()
 
         ImageUtils.loadImage(context, name, holder.itemView.iv_image)
+    }
+
+    private fun getSelectedOptions(soldItem: CustomerData.SoldItem): String {
+        val categories = soldItem.optionCategory ?: return ""
+        if (categories.isEmpty()) return ""
+        val options = StringBuilder()
+        categories.forEach { category ->
+            if (category.title.isNotEmpty()) {
+                category.optionList.forEach { option ->
+                    if (option.isSelected) options.append(option.title).append(" ")
+                }
+            }
+        }
+        return options.toString()
     }
 
     fun setList(items: List<CustomerData.SoldItem>) {
